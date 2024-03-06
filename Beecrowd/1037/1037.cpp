c0cce19d95ce71275e47121d579c5a05e715f799
guiaraujoreal <joaoguilhermearaujo1617@gmail.com>
2024-03-06 09:54:35 -0300

    Envio ex1037 - Beecrowd

---
+++ Beecrowd/1037/1037.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    float x;
+
+    cin >> x;
+    
+    if (x >= 0 && x <=25) cout << "Intervalo [0,25]" << endl;
+    else if (x > 25 && x <= 50) cout << "Intervalo (25,50]" << endl;
+    else if (x > 75 && x <= 100) cout << "Intervalo (75,100]" << endl;
+    else cout << "Fora de intervalo" << endl;
+    
+    return 0;
+}
