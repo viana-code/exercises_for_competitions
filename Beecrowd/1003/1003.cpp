26a87cc6f210924f610770f63badb5eb701d4a79
guiaraujoreal <joaoguilhermearaujo1617@gmail.com>
2024-03-07 10:53:41 -0300

    Envio do ex1003 - Beecrowd

---
+++ Beecrowd/1003/1003.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b, soma;
+
+    cin >> a >> b;
+
+    soma = a + b;
+
+    cout << "SOMA = " << soma << endl;
+
+    return 0;
+}
