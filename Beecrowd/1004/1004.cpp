8f4cbba07088462bfa2bf3de75e216167c763596
viana-code <joaoguilhermearaujo1617@gmail.com>
2024-09-28 11:39:32 -0300

    Add ex 'Beecrowd - 1004'

--- Beecrowd/1004/1004.cpp
+++ Beecrowd/1004/1004.cpp
@@ -8,6 +8,11 @@ int main() {
     ios::sync_with_stdio(0); // Opcional
     cin.tie(0); // Opcional
 
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << "PROD = " << a * b << endl;
     
     return 0;
 }
