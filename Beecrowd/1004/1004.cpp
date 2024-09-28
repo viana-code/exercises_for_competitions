4787bc0385d23ab5ef7930dfb9c5d31eae2999e8
viana-code <joaoguilhermearaujo1617@gmail.com>
2024-09-28 11:30:05 -0300

    Mudanca arquivo de estrutura

---
+++ Beecrowd/1004/1004.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll; // Opcional
+
+int main() {
+    ios::sync_with_stdio(0); // Opcional
+    cin.tie(0); // Opcional
+
+    
+    return 0;
+}
